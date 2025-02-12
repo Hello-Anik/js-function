f5d2231a07d930276e9735d8f89b94179e5aed35
Alexanik12 <afrintitu48@gmail.com>
2025-02-12 06:37:34 -0800

    added return .js

---
+++ return.js
@@ -0,0 +1,13 @@
+function tenTimes(num1){
+    const result = num1 * 10;
+    // console.log(num1,'ten times is', result)
+    return result
+}
+
+tenTimes(200);
+
+const outPut = tenTimes(200);
+console.log('output', outPut)
+
+const ohh = tenTimes(59);
+console.log(ohh)
